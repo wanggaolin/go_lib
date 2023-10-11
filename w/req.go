7460838d84d0a2926f67869d90e9c6f2c7f106bf
Gaolin <gaolin@6.cn>
2023-10-11 11:10:20 +0800

    add request method

--- w/req.go
+++ w/req.go
@@ -19,7 +19,7 @@ func init() {
 type requests struct {
 }
 
-func (r requests) Post(host string, post string, timeout_size int64) (string, int, error) {
+func (r *requests) Post(host string, post string, timeout_size int64) (string, int, error) {
 	timeout := time.Duration(timeout_size) * time.Second
 	client := http.Client{
 		Timeout: timeout,
@@ -34,4 +34,22 @@ func (r requests) Post(host string, post string, timeout_size int64) (string, in
 		return "", 0, err
 	}
 	return string(body), resp.StatusCode, nil
+}
+
+func (r *requests) Head(host string, timeout_size int64) (header *http.Response, err error) {
+	timeout := time.Duration(timeout_size) * time.Second
+	client := http.Client{
+		Timeout: timeout,
+	}
+	req, err := http.NewRequest("HEAD", host, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+	if err != nil {
+		return header, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return header, err
+	}
+	defer resp.Body.Close()
+	return resp, err
 }
