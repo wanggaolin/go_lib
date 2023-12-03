9181086ebdab783ef7159e8160a5ce7e48843615
Gaolin <gaolin@6.cn>
2023-12-03 14:48:44 +0800

    update

--- w/set.go
+++ w/set.go
@@ -6,7 +6,7 @@ func init() {
 	Set = &set{}
 }
 
-//求并集
+// 求并集
 func (s set) union(slice1, slice2 []string) []string {
 	m := make(map[string]int)
 	for _, v := range slice1 {
@@ -22,7 +22,7 @@ func (s set) union(slice1, slice2 []string) []string {
 	return slice1
 }
 
-//求交集
+// 求交集
 func (s set) IntersectInt64(slice1, slice2 []int64) []int64 {
 	m := make(map[int64]int64)
 	nn := make([]int64, 0)
@@ -38,7 +38,7 @@ func (s set) IntersectInt64(slice1, slice2 []int64) []int64 {
 	return nn
 }
 
-//求差集
+// 求差集
 func (s set) DifferenceInt64(slice1, slice2 []int64) []int64 {
 	m := make(map[int64]int64)
 	nn := make([]int64, 0)
@@ -58,6 +58,27 @@ func (s set) DifferenceInt64(slice1, slice2 []int64) []int64 {
 // 列表去重
 func (s set) ArryStringSet(arr []string) (newArr []string) {
 	newArr = make([]string, 0)
+	if arr == nil {
+		return newArr
+	}
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return newArr
+}
+
+// 列表去重
+func (s set) ArryInt64Set(arr []int64) (newArr []int64) {
+	newArr = make([]int64, 0)
 	if arr == nil {
 		return newArr
 	}
