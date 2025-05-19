178ca639930098695f8239c2720c898ff0b97624
Gaolin <gaolin@6.cn>
2025-05-19 15:59:00 +0800

    update

--- w/arry.go
+++ w/arry.go
@@ -39,41 +39,41 @@ func (r *arry) Is_exis_arry_int(name int, atty []int) bool {
 }
 
 // 获取列表最大
-func (r *arry) GetMaxInt(arr []int) int {
+func (r *arry) GetMaxInt(arr []int) (maxInt int) {
 	if len(arr) == 0 {
 		return 0
 	}
-	max := arr[0]
+	maxInt = arr[0]
 	for _, num := range arr {
-		if num > max {
-			max = num
+		if num > maxInt {
+			maxInt = num
 		}
 	}
-	return max
+	return maxInt
 }
 
-func (r *arry) GetMaxInt64(arr []int64) int64 {
+func (r *arry) GetMaxInt64(arr []int64) (maxInt int64) {
 	if len(arr) == 0 {
 		return 0
 	}
-	max := arr[0]
+	maxInt = arr[0]
 	for _, num := range arr {
-		if num > max {
-			max = num
+		if num > maxInt {
+			maxInt = num
 		}
 	}
-	return max
+	return maxInt
 }
 
-func (r *arry) GetMaxFloat64(arr []float64) float64 {
+func (r *arry) GetMaxFloat64(arr []float64) (maxFloat float64) {
 	if len(arr) == 0 {
 		return 0
 	}
-	max := arr[0]
+	maxFloat = arr[0]
 	for _, num := range arr {
-		if num > max {
-			max = num
+		if num > maxFloat {
+			maxFloat = num
 		}
 	}
-	return max
+	return maxFloat
 }
